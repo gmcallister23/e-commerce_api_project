f15535d721f7f9494d5a3bfba0f2a32d6085c4ff
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-03-08 17:21:29 -0400

    Set up models

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from sqlalchemy import Table, Column, Integer, String, ForeignKey, select
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, Float, DateTime, func, select
 from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
 from marshmallow import ValidationError 
 from typing import List, Optional
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -32,12 +33,31 @@ order_product = Table(
 
 #Models
 #User Table
-
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    address: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[Optional[str]] = mapped_column(String(200), unique=True)
+    #One to many - user to orders
+    orders = Mapped[List['Order']] = relationship('Order', back_populates='user')
 #Product table
-
+class Product(Base):
+    __tablename__ = 'products'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    product_name: Mapped[str] = mapped_column(String(30), nullable=False)
+    price: Mapped[float] = mapped_column(Float(5), nullable=False)
+    #Many-many - orders and products
+    orders: Mapped[List['Order']] = relationship('Order', secodary=order_product, back_populates='products')
 #Order table
-
-
+class Order(Base):
+    __tablename__ = 'orders'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    order_date: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    #One to many - user --> orders
+    user = Mapped['User'] = relationship('User', back_populates='orders')
+    products = Mapped[List['Product']] = relationship('Product', secondary=order_product, back_populates='orders')
 
 #Schemas
 #User Schema
